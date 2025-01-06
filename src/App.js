be8d038c18d8227c936577fa2ab2b6d873a7e285
dagiStark <160214504+dagiStark@users.noreply.github.com>
2025-01-06 11:39:28 +0300

    update the routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./pages/auth/Index";
+import ExpenseTracker from "./pages/expense-tracker/Index";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Auth />} />
+        <Route path="/expense-tracker" exact element={<ExpenseTracker />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
